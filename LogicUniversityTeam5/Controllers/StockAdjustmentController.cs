89d55961ab0229a19ad4cd809e3947a32f496c91
Khim22 <khimyang22@gmail.com>
2018-07-18 09:01:29 +0800

    added one controller per use case

---
+++ LogicUniversityTeam5/Controllers/StockAdjustmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LogicUniversityTeam5.Controllers
+{
+    public class StockAdjustmentController : Controller
+    {
+        // GET: StockAdjustment
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
