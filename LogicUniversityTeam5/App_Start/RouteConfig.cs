d65f3ab7e9d59d6ae538c85c11662e0ce3c34e44
Khim22 <khimyang22@gmail.com>
2018-07-18 09:59:46 +0800

    changed to default routes for easy configuration

--- LogicUniversityTeam5/App_Start/RouteConfig.cs
+++ LogicUniversityTeam5/App_Start/RouteConfig.cs
@@ -13,17 +13,17 @@ namespace LogicUniversityTeam5
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Requisition",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "RequisitionController", action = "ManageRequisition", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute(
+            //    name: "Requisition",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "RequisitionController", action = "ManageRequisition", id = UrlParameter.Optional }
+            //);
 
-            routes.MapRoute(
-                name: "Admin",
-                url: "admin/{controller}/{action}/{id}",
-                defaults: new { controller = "HomeController", action = "Index", id = UrlParameter.Optional }
-            );
+            //routes.MapRoute(
+            //    name: "Admin",
+            //    url: "admin/{controller}/{action}/{id}",
+            //    defaults: new { controller = "HomeController", action = "Index", id = UrlParameter.Optional }
+            //);
 
             routes.MapRoute(
                 name: "Default",
