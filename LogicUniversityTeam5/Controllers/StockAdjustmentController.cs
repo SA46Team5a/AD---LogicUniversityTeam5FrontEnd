3a4a70c6569cec57ac6eb9925742f78ca59fe427
Khim22 <khimyang22@gmail.com>
2018-07-18 21:19:15 +0800

    (StockManagement): semi-completed

--- LogicUniversityTeam5/Controllers/StockAdjustmentController.cs
+++ LogicUniversityTeam5/Controllers/StockAdjustmentController.cs
@@ -12,16 +12,38 @@ namespace LogicUniversityTeam5.Controllers
 
     public class StockAdjustmentController : Controller
     {
-
+        //IRetrieveStockManagementService stockRetrieveService;
+        //IUpdateStockManagementService stockUpdateService;
         public ActionResult ManageMonthlyStockDiscrepancy()
         {
+            //stockService =  new RetrieveStockManagementService();
+            //List<StockVoucher> vouchers = stockService.getOpenVouchers();
+            //combine.item = db.Items.ToList;
+            //foreach(StockVoucher v in vouchers)
+            //{ ;
+            //  combine.isSelected.Add(new Dictionary<int,bool>{
+            //     {v.ItemId, false }
+            //  }); }
             ItemAndVoucher combine = new ItemAndVoucher();
             combine.item = getitems();
             combine.stockVoucher = getstockvoucher();
             return View(combine);
-
         }
 
+        [HttpPost]
+        public ActionResult ManageMonthlyStockDiscrepancy(ItemAndVoucher model)
+        {
+            //stockUpdateService =  new UpdateStockManagementService();
+            //List<StockVoucher> vouchers = stockService.getOpenVouchers();
+            //foreach(StockVoucher v in vouchers)
+            //{ combine.item.Add(db.Items.ItemName)}
+            ItemAndVoucher combine = new ItemAndVoucher();
+            combine.item = getitems();
+            combine.stockVoucher = getstockvoucher();
+            return View("Home");
+        }
+
+        // Retrieving Mock Data from here
         public List<Items> getitems()
         {
             List <Items> items = new List<Items>();
---
+++ LogicUniversityTeam5Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LogicUniversityTeam5Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LogicUniversityTeam5Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af124ad6-82c2-41e3-8992-972c2120be77")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
