724f2d7b911dc6e14c68560e2bb982671d503cd3
Khim22 <khimyang22@gmail.com>
2018-07-19 11:18:02 +0800

    corrected routing for StockAdjustment

--- LogicUniversityTeam5/Models/ItemAndVoucher.cs
+++ LogicUniversityTeam5/Models/ItemAndVoucher.cs
@@ -9,7 +9,7 @@ namespace LogicUniversityTeam5.Models
     {
         public List<Items> item { get; set; }
         public List<StockVoucher> stockVoucher { get; set; }
-        public List<Dictionary<int,bool>> isSelected { get; set; }
+        public Dictionary<int,bool> isSelected { get; set; }
 
     }
 }
