65b5fbf18451af9e7b4f6145fd84fc0cee28b828
meitingtonia <meitingtonia@gmail.com>
2018-07-19 14:35:44 +0800

    do cancel button

--- LogicUniversityTeam5/Controllers/CreateOrdersController.cs
+++ LogicUniversityTeam5/Controllers/CreateOrdersController.cs
@@ -10,58 +10,58 @@ namespace LogicUniversityTeam5.Controllers.Order
     public class CreateOrdersController : Controller
     {
         // GET: ItemCatalogue
-        public ActionResult ItemCatalogue()
-        {
-            ordertest combine = new ordertest();
-            combine.order = getorderitems();
-            return View(combine);
-        }
-        public List<OrderItem> getorderitems()
-        {
-            List<OrderItem> orderitem = new List<OrderItem>();
-            {
-                new OrderItem
-                {
-                    category = "Puncher",
-                    description = "2 Holes",
-                    reorderlevel = 20,
-                    currentstock = 17,
-                    recorderquantity = 40
-                };
-                new OrderItem
-                {
-                    category = "Ruler",
-                    description = "Ruler 12'",
-                    reorderlevel = 30,
-                    currentstock = 39,
-                    recorderquantity = 50
-                };
-                new OrderItem
-                {
-                    category = "pen",
-                    description = "2B pencil",
-                    reorderlevel = 50,
-                    currentstock = 20,
-                    recorderquantity = 50
-                };
-                new OrderItem
-                {
-                    category = "pen",
-                    description = "Blue pen",
-                    reorderlevel = 50,
-                    currentstock = 40,
-                    recorderquantity = 50
-                };
-                new OrderItem
-                {
-                    category = "Ruler",
-                    description = "Ruler 6'",
-                    reorderlevel = 30,
-                    currentstock = 38,
-                    recorderquantity = 50
-                };
-                return orderitem;
-            }
-        }
+        //public ActionResult ItemCatalogue()
+        //{
+        //    ordertest combine = new ordertest();
+        //    combine.order = getorderitems();
+        //    return View(combine);
+        //}
+        //public List<OrderItem> getorderitems()
+        //{
+        //    List<OrderItem> orderitem = new List<OrderItem>();
+        //    {
+        //        new OrderItem
+        //        {
+        //            category = "Puncher",
+        //            description = "2 Holes",
+        //            reorderlevel = 20,
+        //            currentstock = 17,
+        //            recorderquantity = 40
+        //        };
+        //        new OrderItem
+        //        {
+        //            category = "Ruler",
+        //            description = "Ruler 12'",
+        //            reorderlevel = 30,
+        //            currentstock = 39,
+        //            recorderquantity = 50
+        //        };
+        //        new OrderItem
+        //        {
+        //            category = "pen",
+        //            description = "2B pencil",
+        //            reorderlevel = 50,
+        //            currentstock = 20,
+        //            recorderquantity = 50
+        //        };
+        //        new OrderItem
+        //        {
+        //            category = "pen",
+        //            description = "Blue pen",
+        //            reorderlevel = 50,
+        //            currentstock = 40,
+        //            recorderquantity = 50
+        //        };
+        //        new OrderItem
+        //        {
+        //            category = "Ruler",
+        //            description = "Ruler 6'",
+        //            reorderlevel = 30,
+        //            currentstock = 38,
+        //            recorderquantity = 50
+        //        };
+        //        return orderitem;
+        //    }
+        //}
     }
 }
