e93ad6217605e79d66d0b4b0952deff7bf477670
Khim22 <khimyang22@gmail.com>
2018-07-19 13:38:55 +0800

    init serviceLayer

--- LogicUniversityTeam5/Controllers/StockAdjustmentController.cs
+++ LogicUniversityTeam5/Controllers/StockAdjustmentController.cs
@@ -1,17 +1,24 @@
-﻿using LogicUniversityTeam5.Models;
+﻿//using LogicUniversityTeam5.Models;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ServiceLayer;
+using ServiceLayer.DataAccess;
 
 namespace LogicUniversityTeam5.Controllers
 {
-
-
+    // Author: Tan Khim Yang, Gong Meiting
     public class StockAdjustmentController : Controller
     {
+        IStockManagementService stockManagementService;
+        public StockAdjustmentController(IStockManagementService sms)
+        {
+            stockManagementService = sms;
+        }
+        
         //IRetrieveStockManagementService stockRetrieveService;
         //IUpdateStockManagementService stockUpdateService;
         public ActionResult ManageMonthlyStockDiscrepancy()
@@ -22,11 +29,12 @@ namespace LogicUniversityTeam5.Controllers
             //foreach(StockVoucher v in vouchers)
             //{ 
             //  combine.isSelected.Add(v.ItemId, false); }
-            ItemAndVoucher combine = new ItemAndVoucher();
-            combine.item = getitems();
-            combine.stockVoucher = getstockvoucher();
-            combine.isSelected = getIsSelected();
-            return View(combine);
+            List<StockVoucher> vouchers = stockManagementService.getOpenVouchers();
+            //ItemAndVoucher combine = new ItemAndVoucher();
+            //combine.item = getitems();
+            //combine.stockVoucher = getstockvoucher();
+            //combine.isSelected = getIsSelected();
+            return View(vouchers);
         }
 
 
@@ -45,27 +53,27 @@ namespace LogicUniversityTeam5.Controllers
         }
 
         // Retrieving Mock Data from here
-        public List<Items> getitems()
-        {
-            List <Items> items = new List<Items>();
-            items.Add(
-                new Items() { ItemName = "2B Pencil" });
-            return items;
-        }
+        //public List<Items> getitems()
+        //{
+        //    List <Items> items = new List<Items>();
+        //    items.Add(
+        //        new Items() { ItemName = "2B Pencil" });
+        //    return items;
+        //}
 
-        public List<StockVoucher> getstockvoucher()
-        {
-            List<StockVoucher> stock = new List<StockVoucher>();
-            stock.Add(new StockVoucher() { discrepancyId = "Z123", actualcount = 95, originalcount = 100,
-                ItemCost = 25, Reason = "damaged", RaisedBy = "Tonia", isNewlyEnrolled = true, RaisedByDate = "18-07-2018",
-            Password = "password"
-            });
-            return stock;
-        }
+        //public List<StockVoucher> getstockvoucher()
+        //{
+        //    List<StockVoucher> stock = new List<StockVoucher>();
+        //    stock.Add(new StockVoucher() { discrepancyId = "Z123", actualcount = 95, originalcount = 100,
+        //        ItemCost = 25, Reason = "damaged", RaisedBy = "Tonia", isNewlyEnrolled = true, RaisedByDate = "18-07-2018",
+        //    Password = "password"
+        //    });
+        //    return stock;
+        //}
 
-        private Dictionary<int, bool> getIsSelected()
-        {
-            return new Dictionary<int, bool>() { { 1, false } };
-        }
+        //private Dictionary<int, bool> getIsSelected()
+        //{
+        //    return new Dictionary<int, bool>() { { 1, false } };
+        //}
     }  
 }
