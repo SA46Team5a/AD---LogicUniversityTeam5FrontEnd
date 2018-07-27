6a5d136eff8cd291c30056daa3d928670bc66b71
Jack <jsim89@gmail.com>
2018-07-27 10:19:04 +0800

    feat(OrdersApi): added endpoints for getting order ids and suppliers

---
+++ LogicUniversityTeam5/ApiControllers/OrdersApiController.cs
@@ -0,0 +1,24 @@
+﻿using ServiceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace LogicUniversityTeam5
+{
+    public class OrdersApiController : ApiController
+    {
+        private readonly IOrderService _orderService = new OrderService();
+
+        [HttpGet]
+        [Route("api/orders/suppliers")]
+        public List<SupplierPayload> getSuppliers()
+            => SupplierPayload.ConvertEntityToPayload(_orderService.getSuppliers());
+
+        [HttpGet]
+        [Route("api/orders/orderids")]
+        public List<OrderPayload> getOutstandingOrderIds()
+            => OrderPayload.ConvertEntityToPayload(_orderService.getOutstandingOrders());
+    }
+}
