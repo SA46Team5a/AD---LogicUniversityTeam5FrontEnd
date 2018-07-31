acd4ddcc949597670e17c1ba4799114ae155362b
pav <Pavana26@github.com>
2018-07-31 14:06:44 +0800

    fixed bug for ViewStationeryCatalogue

--- LogicUniversityTeam5/Controllers/RequisitionController.cs
+++ LogicUniversityTeam5/Controllers/RequisitionController.cs
@@ -76,7 +76,8 @@ namespace LogicUniversityTeam5.Controllers.Requisition
             //Looping each item in view
             for (int i = 0; i < model.Items.Count; i++)
             {
-                string textBoxValue = model.AddedText[i].ToString().Trim();
+                string textBoxValue = null;
+                textBoxValue = model.AddedText[i].ToString().Trim();
                 string itemIdInView = model.Items[i].ItemID;
                 //if item is existing item in requistion, add to quantity
                 if (existingReqDetailsItemIds.Contains(itemIdInView) && (textBoxValue != null && textBoxValue != ""))
